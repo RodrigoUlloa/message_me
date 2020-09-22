216c1f4f46ab008e6f9afc90aa1f24eb170711ac
Rodrigo <rodrigo.ulloa22@gmail.com>
2020-09-21 21:31:48 -0300

    beuty input box, functionality and create custom scope

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,10 +23,20 @@ scroll_bottom = function() {
   }
 }
 
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.KeyCode == 13) {
+      $('button').click();
+      $('#message_body').empty();
+    };
+  });
+};
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submit_message();
   scroll_bottom();
 })
